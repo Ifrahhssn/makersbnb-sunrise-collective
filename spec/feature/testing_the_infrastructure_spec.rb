9c91ca209121bce0f9e24c932b3eb51f0c6f8043
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-21 17:01:26 +0100

    Adding design changes to the index page with CSS

--- spec/feature/testing_the_infrastructure_spec.rb
+++ spec/feature/testing_the_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'Shows index page' do 
   it 'loads the index page properly' do
     visit('/')
-    expect(page).to have_content "Sunrise Bookings!"
+    expect(page).to have_content "Welcome"
   end 
 end
