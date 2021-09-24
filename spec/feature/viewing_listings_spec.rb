e877f8e954d16425ec26d664f7843bc195c0f6f7
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-24 14:53:48 +0100

    Finalising adding more details to a new listing - no failures

--- spec/feature/viewing_listings_spec.rb
+++ spec/feature/viewing_listings_spec.rb
@@ -1,11 +1,13 @@
 require 'pg'
+require_relative '../../lib/listing'
 
 feature 'Shows listings' do 
   it 'shows all the listings on a page' do 
-    Listing.create(listing_name: "Listing number 1")
+    Listing.create(listing_name: "Listing number 1", description: "Listing description", price: "50")
 
     visit('/listings')
     expect(page).to have_content "Listing number 1"
-
+    expect(page).to have_content "Listing description"
+    expect(page).to have_content "50"
   end 
 end
--- spec/unit/listing_spec.rb
+++ spec/unit/listing_spec.rb
@@ -4,21 +4,30 @@ describe Listing do
   describe '.all' do
     it "should show all listings" do 
       connection = PG.connect(dbname: 'sunrise_bnb_manager_test')
-      connection.exec("INSERT INTO listings (listing_name) VALUES ('Listing number 1');")
-      connection.exec("INSERT INTO listings (listing_name) VALUES ('Listing number 2');")
+      connection.exec("INSERT INTO listings (listing_name, description, price) VALUES ('Listing number 1', 'Description 1', '50');")
+      # connection.exec("INSERT INTO listings (listing_name) VALUES ('Listing number 2');")
 
       listings = Listing.all
-      expect(listings).to include "Listing number 1"
-      expect(listings).to include "Listing number 2"
+
+      expect(listings.first.listing_name).to include "Listing number 1"
+      expect(listings.first.description).to include "Description 1"
+      expect(listings.first.price).to include "50"
+      # expect(listings[1].listing_name).to include "Listing number 2"
     end 
   end
 
   describe '.create' do
     it 'creates a new listing' do
-      Listing.create(listing_name: 'Example listing 1', description: 'Example description', price: '60')
-      expect(Listing.all).to include 'Example listing 1'
-      expect(Listing.all).to include 'Example description'
-      expect(Listing.all).to include '60'
+      first_input = Listing.create(listing_name: 'Example listing 1', description: 'Example description', price: '60')
+      
+  
+      persisted_data = PG.connect(dbname: 'sunrise_bnb_manager_test').query("SELECT * FROM listings WHERE 
+      id = #{first_input.id} ;")
+  
+      
+      expect(first_input.id).to eq persisted_data.first['id']
+      # expect(Listing.all).to include 'Example description'
+      # expect(Listing.all).to include '60'
     end
   end
 end
